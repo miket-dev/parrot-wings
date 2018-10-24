f3e8fc930216f5d78e764e2a4f5ed17ca9849a4e
mneofit <m_neo2@mail.ru>
2018-10-24 17:56:37 +0500

    build and start

--- front-angular/webpack.config.js
+++ front-angular/webpack.config.js
@@ -1,80 +1,67 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const helpers = require('../helpers');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const helpers = require('./config/helpers');
 
 module.exports = {
-    devtool: 'cheap-module-eval-source-map',
-    output: {
-        path: helpers.root('dist'),
-        publicPath: 'http://localhost:3000/',
-        filename: '[name].js',
-        chunkFilename: '[id].chunk.js'
-    },
-    devServer: {
-        historyApiFallback: true,
-        stats: 'minimal'/*,
-        proxy: {
-            '/api/**': {
-                target: 'http://localhost:8080/your-rest-service',
-                secure: false,
-                changeOrigin: true
-            }
-        }*/
-    },
-    entry: {
-        'polyfills': './src/polyfills.ts',
-        'vendor': './src/vendor.ts',
-        'app': './src/main.ts',
-        'styles': './src/global.scss'
-    },
+  entry: {
+    'polyfills': './src/polyfills.ts',
+    'vendor': './src/vendor.ts',
+    'app': './src/main.ts'
+  },
 
-    resolve: {
-        extensions: [
-            '.js', '.ts'
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loaders: [
+          {
+            loader: 'awesome-typescript-loader',
+            options: { configFileName: helpers.root('config', 'tsconfig.json') }
+          } , 'angular2-template-loader'
         ]
-    },
-
-    module: {
-        rules: [{
-                test: /\.ts$/,
-                use: ['awesome-typescript-loader?configFileName=config/tsconfig.json', 'angular2-template-loader']
-            },
-            {
-                test: /\.html$/,
-                use: 'html-loader'
-            },
-            {
-                test: /\.scss$/,
-                exclude: [/node_modules/, helpers.root('src', 'global.scss')],
-                use: ['to-string-loader', 'css-loader', 'sass-loader']
-            },
-            {
-                test: /global\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    use: 'css-loader!sass-loader'
-                })
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg|woff|woff2|otf|ttf|eot|ico)$/,
-                use: 'file-loader?name=assets/[name].[hash].[ext]'
-            }
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'file-loader?name=assets/[name].[hash].[ext]'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "text-loader",
+          "style-loader",
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
         ]
-    },
-
-    plugins: [
-        new webpack.optimize.CommonsChunkPlugin({
-            name: ['app', 'vendor', 'polyfills']
-        }),
-
-        new HtmlWebpackPlugin({
-            template: 'src/index.html'
-        }),
-
-        new webpack.ContextReplacementPlugin(
-            /angular(\\|\/)core(\\|\/)@angular/,
-            helpers.root('src'), {}
-        ),
-        new ExtractTextPlugin('styles.css')
+      }
     ]
+  },
+
+  plugins: [
+    // Workaround for angular/angular#11580
+    new webpack.ContextReplacementPlugin(
+      // The (\\|\/) piece accounts for path separators in *nix and Windows
+      /angular(\\|\/)core(\\|\/)@angular/,
+      helpers.root('./src'), // location of your src
+      {} // a map of your routes
+    ),
+
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: ['app', 'vendor', 'polyfills']
+    // }),
+
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
 };
